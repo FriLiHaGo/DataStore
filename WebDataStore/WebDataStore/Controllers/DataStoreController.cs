98a8450eaa8b23ef7114d99d5a6784170f3ab5ef
Погудин Миша <pogudin_m@mail.ru>
2018-02-07 05:13:27 +0300

    Макет

---
+++ WebDataStore/WebDataStore/Controllers/DataStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDataStore.Controllers
+{
+    public class DataStoreController : Controller
+    {
+        // GET: DataStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebDataStore/WebDataStore/Extensions/Helper.cs
@@ -0,0 +1,16 @@
+﻿using DataStoreDB.NHibernate.NHRepositories;
+using System.Web.Mvc;
+
+namespace WebDataStore.Extensions
+{
+    public static class DSHelper
+    {
+        public static MvcHtmlString GetFIO(this HtmlHelper html)
+        {
+            var userRepository = new NHUserRepository();
+            var name = html.ViewContext.HttpContext.User.Identity.Name;
+            var user = userRepository.GetByLogin(name);
+            return MvcHtmlString.Create($"{user.Name} {user.Sername}");
+        }
+    }
+}
